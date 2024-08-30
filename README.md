b97b3eb36bb1efc7ba8c3731808b91895ee00056
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-08-30 13:27:12 -0400

    Initial POC of docusaur + submodules

--- README.md
+++ README.md
@@ -6,9 +6,76 @@ To clone the repository with the files in the submodules run:
 git clone --recurse-submodules https://github.com/MitchellShiell/bridge.git
 ```
 
-Once cloned make sure the submodules are up to date:
+To pull a new submodule into your project after it has been added to the `.gitmodules` file, you can follow these steps:
+
+## Initializing the new submodules
+
+Run the following command to initialize the new submodule:
 
 ```bash
-git submodule update --remote
+git submodule update --init --recursive
 ```
 
+This command initializes all submodules, including the new one, and clones them into your project [1].
+
+### Step 3: Fetch the latest changes
+
+Fetch the latest changes for all submodules, including the new one:
+
+```bash
+git fetch --recurse-submodules
+```
+
+This command fetches the latest changes for all submodules [1].
+
+### Step 4: Pull the new submodule
+
+Now, you can pull the new submodule using:
+
+```bash
+git submodule update --recursive --remote
+```
+
+This command pulls the latest changes for all submodules, including the new one [1].
+
+# Docusaurus
+
+This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ yarn
+```
+
+### Local Development
+
+```
+$ yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+Using SSH:
+
+```
+$ USE_SSH=true yarn deploy
+```
+
+Not using SSH:
+
+```
+$ GIT_USER=<Your GitHub username> yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ docusaurus.config.ts
@@ -0,0 +1,141 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'OvertureDev',
+  tagline: 'Creating applications to store, manage, and distribute massive genomic datasets',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://overture-dev.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'overture-stack', // Usually your GitHub org/user name.
+  projectName: 'OvertureDev', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/overture-stack/OvertureDev',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/overture-stack/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'OvertureDev',
+      logo: {
+        alt: 'Overture Dev Logo',
+        src: 'img/Overture-logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/overture-stack',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Developer Documentation',
+              to: '/docs/intro',
+            },
+            {
+              label: 'Product Documentation',
+              to: 'https://www.overture.bio/getting-started/',
+            },
+            {
+              label: 'Platform Guides',
+              to: 'https://www.overture.bio/documentation/guides/',
+            }
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Slack',
+              href: 'https://join.slack.com/t/overture-bio/shared_invite/zt-21tdumtdh-9fP1TFeLepK4~Lc377rOYw',
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/overture-stack',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Overture, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
