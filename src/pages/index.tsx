4089c0232508c662ccedc4c5ac63145b1bfee9e7
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-08-30 15:36:11 -0400

    updated link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/moduleA">
+            to="/docs/moduleA/intro">
             OvertureDev KickStart Guide - 15min ⏱️
           </Link>
         </div>
