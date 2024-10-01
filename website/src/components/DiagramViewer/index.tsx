c32fba9e90a229c3765b61d011f93f06696ead41
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-10-01 17:48:49 -0400

    minor updates

---
+++ website/src/components/DiagramViewer/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+import Platform from '/.image/Overture-Platform';
+import StageDev from '/.image/Overture-StageDev';
+import ArrangerDev from '/.image/Overture-ArrangerDev';
+import MaestroDev from '/.image/Overture-MaestroDev';
+import SongDev from '/.image/Overture-SongDev';
+import ScoreDev from '/.image/Overture-ScoreDev';
+
+interface DiagramViewer {
+    image: string;
+}
+
+export default function DiagramViewer() {
+   const diagramMenu -
+
+    return (
+
+
+    );
+}
