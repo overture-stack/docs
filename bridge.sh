1f317462bb2209b099979bff4482c98cf79aee88
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-10-03 15:59:10 -0400

    i think its fixed  had enough

--- bridge.sh
+++ bridge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# When update or making new symbolic links be sure to use absolute paths
+# Do not use absolute paths, they will not deploy on netlify correctly
 
-ln -s ../../submodules/score/docs website/docs/03-Score
-ln -s ../../submodules/song/docs website/docs/02-Song
-ln -s ../../submodules/arranger/docs website/docs/05-Arranger
-ln -s ../../submodules/stage/docs website/docs/06-Stage
-ln -s ../../submodules/maestro/docs website/docs/04-Maestro
+ln -s ../../submodules/song/docs website/docs/01-Song
+ln -s ../../submodules/score/docs website/docs/02-Score
+ln -s ../../submodules/maestro/docs website/docs/03-Maestro
+ln -s ../../submodules/arranger/docs website/docs/04-Arranger
+ln -s ../../submodules/stage/docs website/docs/05-Stage
