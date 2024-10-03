241eb36f7242c2297477ed2ef7e43f0122c26a52
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-10-03 14:47:50 -0400

    updated symlinks

--- bridge.sh
+++ bridge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# When update or making new symbolic links be sure to use absolute paths
+
 ln -s ../../submodules/score/docs website/docs/03-Score
 ln -s ../../submodules/song/docs website/docs/02-Song
 ln -s ../../submodules/arranger/docs website/docs/05-Arranger
