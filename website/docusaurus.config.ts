99b1a2c1940671723d801e732a4ef5f77b6b5497
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-12-11 09:59:43 -0500

    updated social card

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/overture-social-card.png',
     navbar: {
       title: 'Overture Docs',
       logo: {
