94560b1681540dafe9ea7758d2c6657c312fa45f
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-12-19 14:53:25 -0500

    Adding Matomo tracking using matomo plugin take 5

--- website/src/components/MatomoTracking/MatomoTracking.tsx
+++ website/src/components/MatomoTracking/MatomoTracking.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-
-// See https://developer.matomo.org/guides/spa-tracking
 
 declare global {
   interface Window {
@@ -16,28 +15,18 @@ export default function MatomoTracking(): JSX.Element | null {
     }
 
     var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(["setCookieDomain", "*.overture.bio"]);
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
 
-    const u = "https://webstats.oicr.on.ca/piwik/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '76']);
-
-    const d = document;
-    const g = d.createElement('script');
-    const s = d.getElementsByTagName('script')[0];
-    g.async = true; 
-    g.src = u + 'matomo.js';
-    if (s.parentNode) {
-      s.parentNode.insertBefore(g, s);
-    }
-
-    // Track page views in Docusaurus SPA
-    require('@docusaurus/router').default.events.on('routeDidUpdate', () => {
-      _paq.push(['setCustomUrl', window.location.pathname]);
-      _paq.push(['trackPageView']);
-    });
+    (function() {
+      var u="//webstats.oicr.on.ca/piwik/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '76']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
 
   }, []);
 
