a2290266f6a4902e4f0954c71fccdcc751873f12
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2025-04-09 16:09:42 -0400

    Prelude, Quickstart & Docs updates + docker version #'s (#27)
    * Prelude, Quickstart & Libretto updates + docker version #'s
    
    * quickstart.git > -b quickstart ... prelude.git
    
    * updated submodule commits
    
    * swagger ui react version number
    
    ---------
    
    Co-authored-by: Mitchell Shiell <mshiell@ML8164-MSHIELL.local>
    Co-authored-by: Mitchell Shiell <mshiell@wl7053-spatel.ad.oicr.on.ca>

--- website/docs/03-other-software/03-libretto.md
+++ website/docs/03-other-software/03-libretto.md
@@ -1,11 +1,6 @@
+# Libretto
 
-# Bridge
-
-Bridge is a single, easy-to-navigate hub that beautifully renders all our product documentation from the `/docs` folder of all our GitHub repositories. 
-
-:::info 
-Overture Docs started under the name Bridge to reflect its ability to bridge information across multiple repositories. Bridge was also chosen in fitting with Overtures Orchestral theme.
-:::
+Libretto is a single, easy-to-navigate hub that beautifully renders all our product documentation from the `/docs` folder of all our GitHub repositories.
 
 ## Documentation Framework
 
@@ -19,16 +14,16 @@ Overture has three user profiles:
 
 Our documentation is split up as follows:
 
-| Documentation | User Profile | Description
-|---|---|---|
-| Product Documentation (Housed here) | Software Engineers & Developers | Technical resources for those working on or contributing to the project. | 
-| Platform Guides (Overture.bio)  | IT Specialists & Informaticians | Instructive guides covering platform setup, maintenance and usage for end-users and administrators. |
+| Documentation                       | User Profile                    | Description                                                                                         |
+| ----------------------------------- | ------------------------------- | --------------------------------------------------------------------------------------------------- |
+| Product Documentation (Housed here) | Software Engineers & Developers | Technical resources for those working on or contributing to the project.                            |
+| Platform Guides (Overture.bio)      | IT Specialists & Informaticians | Instructive guides covering platform setup, maintenance and usage for end-users and administrators. |
 
 ## How Overture Docs Works
 
 - **Docusaurus**: We use Docusaurus to render the site, providing a sleek and navigable interface for our documentation.
 
-- **Markdown Files**: All documentation content is stored as markdown files in the `/website/docs` directory. 
+- **Markdown Files**: All documentation content is stored as markdown files in the `/website/docs` directory.
 
 - **Git Submodules**: We use Git submodules to store and track all our GitHub repositories within one main repository. All submodules can be found in the `submodule` folder.
 
@@ -44,11 +39,11 @@ Our documentation is split up as follows:
 
 - **Robust Error Handling**: Docusaurus has excellent error catching, particularly for broken and missing links, reducing the need for manual testing.
 
-![Pro Tip](./images/proTip.png 'Use Overture Docs repo to search across all Overture repos')
+![Pro Tip](./images/proTip.png "Use Overture Docs repo to search across all Overture repos")
 
-  :::tip Pro Tip
-  The Overture Docs repo contains everything, therefore finding & tracking links and content across all our repos has never been easier.
-  :::
+:::tip Pro Tip
+The Overture Docs repo contains everything, therefore finding & tracking links and content across all our repos has never been easier.
+:::
 
 ## Getting Started
 
@@ -78,9 +73,9 @@ npm start
 
 Use the following command from the submodules directory to add a new submodule:
 
-   ```bash
-   git submodule add -b <branchName> <GitHub repository URL> module_name
-   ```
+```bash
+git submodule add -b <branchName> <GitHub repository URL> module_name
+```
 
 This will automatically update the `.gitmodules` file located in the root directory.
 
@@ -88,9 +83,9 @@ This will automatically update the `.gitmodules` file located in the root direct
 
 To pull the latest changes for all submodules, including any newly added ones run:
 
-   ```bash
-   git submodule update --remote
-   ```
+```bash
+git submodule update --remote
+```
 
 ### Using Symlinks
 
@@ -98,30 +93,30 @@ All documentation content is stored in markdown files located within `/website/d
 
 Symlinks are used here to link only the necessary folders containing markdown files from the submodule directories.
 
-The script for this is in the root directory, titled `bridge.sh`:
+The script for this is in the root directory, titled `Libretto.sh`:
 
-   ```bash
-   ln -s ../../submodules/song/docs website/docs/Song
-   ln -s ../../submodules/score/docs website/docs/Score
-   ```
+```bash
+ln -s ../../submodules/song/docs website/docs/Song
+ln -s ../../submodules/score/docs website/docs/Score
+```
 
-   This allows us to import only the necessary markdown files that Docusaurus needs. Changes to either directory are linked and reflected in both.
+This allows us to import only the necessary markdown files that Docusaurus needs. Changes to either directory are linked and reflected in both.
 
 #### Getting it to run with Docusaurus
 
 To run this without errors, I needed to create a plugin found in `website/docsPlugin.ts`:
 
 ```typescript
-module.exports = function(context, options) {
+module.exports = function (context, options) {
   return {
     name: "custom-docusaurus-plugin",
     configureWebpack(config, isServer, utils) {
       return {
         resolve: {
-          symlinks: false
-        }
+          symlinks: false,
+        },
       };
-    }
+    },
   };
 };
 ```
@@ -139,16 +134,15 @@ The source of this fix can be found [here](https://github.com/facebook/docusauru
 We can import components by default across all our mdx files reducing the head matter when using these components in mdx documents. This can be configured from the `/src/theme/MDXComponents.ts`:
 
 ```ts title="MDXComponents.ts"
-import type {ComponentType} from 'react';
-import MDXComponents from '@theme-original/MDXComponents';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-
+import type { ComponentType } from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
 
 const components: typeof MDXComponents & {
-    Tabs: ComponentType<any>;
-    TabItem: ComponentType<any>;
-    DocCardList: ComponentType<any>;
+  Tabs: ComponentType<any>;
+  TabItem: ComponentType<any>;
+  DocCardList: ComponentType<any>;
 } = {
   ...MDXComponents,
   Tabs,
@@ -159,8 +153,7 @@ const components: typeof MDXComponents & {
 export default components;
 ```
 
-
-## Custom Components 
+## Custom Components
 
 There are three custom components built for this site all located in the components directory as follows:
 
@@ -182,7 +175,7 @@ There are three custom components built for this site all located in the compone
 - Products are defined in `const products:` array (lines 47-61) with each product requiring:
   - `title`: Product name
   - `link`: URL path
-  - `description`: Brief description 
+  - `description`: Brief description
   - `category`: Must match a category key
   - `image`: Optional icon path
 
@@ -191,12 +184,12 @@ The layout groups products into their respective categories and automatically di
 This component also includes our funding statement, the copy used can be updated directly from the component on line 90.
 
 :::tip Improvement Consideration
-The current implementation uses hardcoded arrays (leftColumnCategories and rightColumnCategories) for column distribution. This is not ideal however it is the simplest method to organize our site content in its ideal layout. 
+The current implementation uses hardcoded arrays (leftColumnCategories and rightColumnCategories) for column distribution. This is not ideal however it is the simplest method to organize our site content in its ideal layout.
 :::
 
 ### Sidebar Funding Statement
 
-A simple React component that displays funding attribution information on the bottom left of all sidebars. It uses CSS Modules for scoped styling (styles.module.css). 
+A simple React component that displays funding attribution information on the bottom left of all sidebars. It uses CSS Modules for scoped styling (styles.module.css).
 
 :::tip Improvement Consideration
 Consider making the content configurable by accepting props rather than hardcoding the grant information, allowing reuse for different funding sources.
@@ -215,6 +208,7 @@ The `SwaggerAPIDoc` component renders API documentation completely client-side b
    - Specifications can be version controlled alongside the code
 
 Usage example with local JSON:
+
 ```jsx
 // Your JSON spec file (e.g., songAPI.json)
 {
--- website/src/components/SiteMap/index.tsx
+++ website/src/components/SiteMap/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import React from "react";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
-import iconSong from './icons/icon-song.png';
-import iconScore from './icons/icon-score.png';
-import iconMaestro from './icons/icon-maestro.png';
-import iconArranger from './icons/icon-arranger.png';
-import iconStage from './icons/icon-stage.png';
-import iconLyric from './icons/icon-lyric.png';
-import iconLectern from './icons/icon-lectern.png';
+import iconSong from "./icons/icon-song.png";
+import iconScore from "./icons/icon-score.png";
+import iconMaestro from "./icons/icon-maestro.png";
+import iconArranger from "./icons/icon-arranger.png";
+import iconStage from "./icons/icon-stage.png";
+import iconLyric from "./icons/icon-lyric.png";
+import iconLectern from "./icons/icon-lectern.png";
 
 interface Product {
   title: string;
@@ -25,57 +25,154 @@ interface Category {
 
 const categories: Record<string, Category> = {
   platform: {
-    title: 'Platform Guides',
-    description: 'Bringing it all together with platform focused guides'
+    title: "Platform Guides",
+    description: "Bringing it all together with platform focused guides",
   },
   core: {
-    title: 'Developer Documentation',
-    description: 'Using our software in a development context'
+    title: "Developer Documentation",
+    description: "Using our software in a development context",
   },
   development: {
-    title: 'Under Development',
-    description: 'New components not quite ready for production'
+    title: "Under Development",
+    description: "New components not quite ready for production",
   },
   supporting: {
-    title: 'Supporting Software',
-    description: 'Supporting tools within the Overture project'
+    title: "Supporting Software",
+    description: "Supporting tools within the Overture project",
   },
   standards: {
-    title: 'Documentation Standards',
-    description: 'Forming the foundation of documentation and development practice'
+    title: "Documentation Standards",
+    description:
+      "Forming the foundation of documentation and development practice",
   },
 };
 
 const products: Product[] = [
-  { title: 'Deployment Guides', link: '/guides/deployment-guide/', description: 'Deploying to Production', category: 'platform' },
-  { title: 'Administration Guides', link: '/guides/administration-guides/', description: 'Management and Customization', category: 'platform' },
-  { title: 'User Guides', link: '/guides/user-guides/', description: 'Interacting with the platform', category: 'platform' },
-  { title: 'API Reference', link: '/guides/api-reference', description: 'Interacting with the platform\'s APIs', category: 'platform' },
-  { title: 'Song', link: 'docs/core-software/song/overview', image: iconSong, description: 'Metadata Management Service', category: 'core' },
-  { title: 'Score', link: '/docs/core-software/score/overview', image: iconScore, description: 'File Transfer Service', category: 'core' },
-  { title: 'Maestro', link: '/docs/core-software/maestro/overview', image: iconMaestro, description: 'Metadata Indexing Service', category: 'core' },
-  { title: 'Arranger', link: '/docs/core-software/arranger/overview', image: iconArranger, description: 'Search API', category: 'core' },
-  { title: 'Stage', link: '/docs/core-software/stage/overview', image: iconStage, description: 'Web Portal Scaffolding', category: 'core' },
-  { title: 'Lectern', link: '/docs/under-development/lectern/', image: iconLectern ,description: 'Schema Management Service', category: 'development' },
-  { title: 'Lyric', link: '/docs/under-development/lyric/', image: iconLyric, description: 'Tabular Data Submission Service', category: 'development' },
-  { title: 'Bridge', link: '/docs/other-software/Bridge', description: 'Documentation Site', category: 'supporting' },
-  { title: 'Conductor', link: 'https://docs.overture.bio/docs/other-software/Conductor', description: 'Software Setup Automation', category: 'supporting' },
-  { title: 'Documenting Projects', link: '/docs/Standards/github', description: 'Organization Standards', category: 'standards' },
-  { title: 'Documenting Software', link: '/docs/Standards/Software/', description: 'Software Standards', category: 'standards' },
+  {
+    title: "Deployment Guides",
+    link: "/guides/deployment-guide/",
+    description: "Deploying to Production",
+    category: "platform",
+  },
+  {
+    title: "Administration Guides",
+    link: "/guides/administration-guides/",
+    description: "Management and Customization",
+    category: "platform",
+  },
+  {
+    title: "User Guides",
+    link: "/guides/user-guides/",
+    description: "Interacting with the platform",
+    category: "platform",
+  },
+  {
+    title: "API Reference",
+    link: "/guides/api-reference",
+    description: "Interacting with the platform's APIs",
+    category: "platform",
+  },
+  {
+    title: "Song",
+    link: "docs/core-software/song/overview",
+    image: iconSong,
+    description: "Metadata Management Service",
+    category: "core",
+  },
+  {
+    title: "Score",
+    link: "/docs/core-software/score/overview",
+    image: iconScore,
+    description: "File Transfer Service",
+    category: "core",
+  },
+  {
+    title: "Maestro",
+    link: "/docs/core-software/maestro/overview",
+    image: iconMaestro,
+    description: "Metadata Indexing Service",
+    category: "core",
+  },
+  {
+    title: "Arranger",
+    link: "/docs/core-software/arranger/overview",
+    image: iconArranger,
+    description: "Search API",
+    category: "core",
+  },
+  {
+    title: "Stage",
+    link: "/docs/core-software/stage/overview",
+    image: iconStage,
+    description: "Web Portal Scaffolding",
+    category: "core",
+  },
+  {
+    title: "Lectern",
+    link: "/docs/under-development/lectern/",
+    image: iconLectern,
+    description: "Schema Management Service",
+    category: "development",
+  },
+  {
+    title: "Lyric",
+    link: "/docs/under-development/lyric/",
+    image: iconLyric,
+    description: "Tabular Data Submission Service",
+    category: "development",
+  },
+  {
+    title: "Quickstart",
+    link: "https://docs.overture.bio/docs/other-software/Quickstart",
+    description: "Locally Deployed Demo Portal",
+    category: "supporting",
+  },
+  {
+    title: "Libretto",
+    link: "/docs/other-software/Libretto",
+    description: "Documentation Site",
+    category: "supporting",
+  },
+  {
+    title: "Prelude",
+    link: "https://docs.overture.bio/docs/other-software/Prelude",
+    description: "Platform planning & development toolkit.",
+    category: "supporting",
+  },
+  {
+    title: "Documenting Projects",
+    link: "/docs/Standards/github",
+    description: "Organization Standards",
+    category: "standards",
+  },
+  {
+    title: "Documenting Software",
+    link: "/docs/Standards/Software/",
+    description: "Software Standards",
+    category: "standards",
+  },
 ];
 
 const Card = ({ title, description, link, image }) => (
   <a href={link} className={styles.card}>
-    {image && <img src={image} alt={`${title} icon`} className={styles.cardImage} />}
-    <Heading as="h4" className={styles.cardTitle}>{title}</Heading>
+    {image && (
+      <img src={image} alt={`${title} icon`} className={styles.cardImage} />
+    )}
+    <Heading as="h4" className={styles.cardTitle}>
+      {title}
+    </Heading>
     <p className={styles.cardDescription}>{description}</p>
   </a>
 );
 
 const CategorySection = ({ category, items }) => (
   <div className={`${styles.categorySection} ${styles[category]}`}>
-    <Heading as="h2" className={styles.categoryHeader}>{categories[category].title}</Heading>
-    <p className={styles.categorySubheader}>{categories[category].description}</p>
+    <Heading as="h2" className={styles.categoryHeader}>
+      {categories[category].title}
+    </Heading>
+    <p className={styles.categorySubheader}>
+      {categories[category].description}
+    </p>
     <div className={styles.cardGrid}>
       {items.map((props, idx) => (
         <Card key={idx} {...props} />
@@ -87,11 +184,19 @@ const CategorySection = ({ category, items }) => (
 const FundingBadge = () => (
   <div className={styles.fundingBadge}>
     <div className={styles.fundingContent}>
-      <Heading as="h3" className={styles.fundingTitle}>Funding Contributors</Heading>
+      <Heading as="h3" className={styles.fundingTitle}>
+        Funding Contributors
+      </Heading>
       <p className={styles.fundingDescription}>
-      Overture is supported by grant #U24CA253529 from the National Cancer Institute at the US National Institutes of Health, and additional funding from Genome Canada, the Canada Foundation for Innovation, the Canadian Institutes of Health Research, Canarie, and the Ontario Institute for Cancer Research.
+        Overture is supported by grant #U24CA253529 from the National Cancer
+        Institute at the US National Institutes of Health, and additional
+        funding from Genome Canada, the Canada Foundation for Innovation, the
+        Canadian Institutes of Health Research, Canarie, and the Ontario
+        Institute for Cancer Research.
       </p>
-      <a href="community/funding" className={styles.fundingLink}>Learn More</a>
+      <a href="community/funding" className={styles.fundingLink}>
+        Learn More
+      </a>
     </div>
   </div>
 );
@@ -102,21 +207,29 @@ const SiteMap = () => {
     return acc;
   }, {});
 
-  const rightColumnCategories = ['platform', 'supporting', 'standards'];
-  const leftColumnCategories = ['core', 'development'];
+  const rightColumnCategories = ["platform", "supporting", "standards"];
+  const leftColumnCategories = ["core", "development"];
 
   return (
     <section className={styles.siteMap}>
       <div className={styles.container}>
         <div className={styles.mosaicLayout}>
           <div className={styles.leftColumn}>
-            {leftColumnCategories.map(category => (
-              <CategorySection key={category} category={category} items={categorizedProducts[category] || []} />
+            {leftColumnCategories.map((category) => (
+              <CategorySection
+                key={category}
+                category={category}
+                items={categorizedProducts[category] || []}
+              />
             ))}
           </div>
           <div className={styles.rightColumn}>
-            {rightColumnCategories.map(category => (
-              <CategorySection key={category} category={category} items={categorizedProducts[category] || []} />
+            {rightColumnCategories.map((category) => (
+              <CategorySection
+                key={category}
+                category={category}
+                items={categorizedProducts[category] || []}
+              />
             ))}
           </div>
         </div>
